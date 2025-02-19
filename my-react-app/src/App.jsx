f460735f8afce7e4c2bb2bf579ac18cce41784b8
sham-sha <sham08092003@gmail.com>
2025-02-19 09:28:45 +0530

    profile card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,31 +1,13 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
-
-import Card from "./Card.jsx";
+import Card from "./Card.jsx"
 
 
-
-// Lesson 1 
-function App() {
+function App(){
   return(
     <>
-    <Header></Header>
-    <Food></Food>
-    <Footer></Footer>
+    <Card/>
     </>
   );
+
 }
-
-
-
-// function App(){
-//   return(
-//     <Card/>
-
-//   );
-
-// }
-
 
 export default App
--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -6,7 +6,7 @@ function Card(){
         <div className="card">
             <img src={Profile} alt="My image"></img>
             <h2>Sam</h2>
-            <p>I Love play and listen music</p>
+            <p>I am a React developer</p>
         </div>
 
     );
